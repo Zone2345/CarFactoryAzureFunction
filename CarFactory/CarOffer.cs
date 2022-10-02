964a8ce44d61844189361809cebb1e7ad915164b
Radovan Jovanovic <radovan.jovanovic@vicert.com>
2022-10-02 20:14:06 +0200

    caroffer method finished

---
+++ CarFactory/CarOffer.cs
@@ -0,0 +1,44 @@
+using CarFactory.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarFactory
+{
+    public class CarOffer
+    {
+
+        [FunctionName("GetCarOffer")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(databaseName: "CarManufactory",
+            collectionName: "Cars",
+            ConnectionStringSetting = "CosmosDbConnectionString")]IEnumerable<Car> getDocument,
+            ILogger log)
+        {
+            string name = req.Query["name"];
+            string model = req.Query["model"];
+            string color = req.Query["color"];
+
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            var manufactory = getDocument.FirstOrDefault(x => x.Manufacturer == name);
+            if (manufactory == null) return new OkObjectResult("Manufacturer " + name + " u are looking for does not exist!");
+            manufactory.Model = getDocument.FirstOrDefault(x => x.Model == model).Model;
+            if (manufactory == null) return new OkObjectResult("Model " + model + " u are looking for does not exist!");
+            var getOffer = getDocument.FirstOrDefault(x => x.Color == color);
+            if(getOffer == null) return new OkObjectResult("Color " + color + " u are looking for does not exist!");
+            manufactory.Hp = getOffer.Hp;
+            manufactory.Engine = getOffer.Engine;
+            manufactory.Fuel = getOffer.Fuel;
+            manufactory.Color = getOffer.Color;
+
+
+            return new OkObjectResult(manufactory);
+        }
+    }
+}
